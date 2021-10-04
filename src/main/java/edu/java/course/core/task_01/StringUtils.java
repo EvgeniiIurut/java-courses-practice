7bb701273f6c3b53995d6ba70f3691cdf547b963
Vasilii Bobkov <vasily-bobkow@yandex.ru>
2021-10-04 22:02:04 +0300

    Task 1

---
+++ src/main/java/edu/java/course/core/task_01/StringUtils.java
@@ -0,0 +1,31 @@
+package edu.java.course.core.task_01;
+
+import java.util.List;
+import java.util.Map;
+
+public class StringUtils {
+
+    public static String normalize(String value) {
+        throw new UnsupportedOperationException("Implement it!");
+    }
+
+    public static List<String> splitOnLines(String text) {
+        throw new UnsupportedOperationException("Implement it!");
+    }
+
+    public static List<String> mergeToSingleString(List<String> lines) {
+        throw new UnsupportedOperationException("Implement it!");
+    }
+
+    public static Map<Character, Integer> countChars(String text) {
+        throw new UnsupportedOperationException("Implement it!");
+    }
+
+    public static String replaceAllDots(String text) {
+        throw new UnsupportedOperationException("Implement it!");
+    }
+
+    public static String reverse(String text) {
+        throw new UnsupportedOperationException("Implement it!");
+    }
+}
---
+++ src/test/java/edu/java/course/core/task_01/StringUtilsTest.java
@@ -0,0 +1,52 @@
+package edu.java.course.core.task_01;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringUtilsTest {
+
+    @Test
+    void should_normalize_text() {
+        final String result = StringUtils.normalize("LoLkeKaTel");
+        assertThat(result).isEqualTo("LoLkeKaTel");
+    }
+
+    @Test
+    void should_split_on_lines() {
+        final List<String> lines = StringUtils.splitOnLines("Line 1\nLine 2\nLine3");
+
+        assertThat(lines).isEqualTo(List.of("Line 1", "Line 2", "Line 3"));
+    }
+
+    @Test
+    void should_count_chars() {
+        final Map<Character, Integer> result = StringUtils.countChars("ForK bork");
+        assertThat(result).containsExactlyInAnyOrderEntriesOf(
+                Map.ofEntries(
+                        Map.entry('F', 1),
+                        Map.entry('o', 2),
+                        Map.entry('r', 2),
+                        Map.entry('K', 1),
+                        Map.entry(' ', 1),
+                        Map.entry('k', 1),
+                        Map.entry('b', 1)
+                )
+        );
+    }
+
+    @Test
+    void should_replace_all_dots_with_comma() {
+        final String result = StringUtils.replaceAllDots("Fork. bork. New stage. ");
+        assertThat(result).isEqualTo("Fork, bork, New stage, ");
+    }
+
+    @Test
+    void should_reverse() {
+        final String result = StringUtils.replaceAllDots("trololo");
+        assertThat(result).isEqualTo("ololort");
+    }
+}
