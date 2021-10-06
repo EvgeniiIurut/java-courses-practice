122b00fd1a95684ec3c9a265a6fe9c5e12c7332d
EvgeniiIurut <65227752+EvgeniiIurut@users.noreply.github.com>
2021-10-06 21:35:11 +0300

    Add files via upload

--- src/main/java/edu/java/course/core/task_01/StringUtils.java
+++ src/main/java/edu/java/course/core/task_01/StringUtils.java
@@ -1,31 +1,54 @@
 package edu.java.course.core.task_01;
 
-import java.util.List;
-import java.util.Map;
+import java.lang.*;
+import java.util.*;
 
 public class StringUtils {
 
     public static String normalize(String value) {
-        throw new UnsupportedOperationException("Implement it!");
+        return value.toLowerCase();
+//        throw new UnsupportedOperationException("Implement it!");
     }
 
     public static List<String> splitOnLines(String text) {
-        throw new UnsupportedOperationException("Implement it!");
+        System.out.println("new ArrayList<>(Arrays.asList(text.split(\"\\n\")))");
+        return new ArrayList<>(Arrays.asList(text.split("\n")));
+
+//        throw new UnsupportedOperationException("Implement it!");
     }
 
     public static String mergeToSingleString(List<String> lines) {
-        throw new UnsupportedOperationException("Implement it!");
+        StringBuilder stringBuilder = new StringBuilder();
+        Iterator<String> iterator = lines.iterator();
+        while (iterator.hasNext()) {
+            stringBuilder.append(iterator.next());
+        }
+        return stringBuilder.toString().trim();
+//        throw new UnsupportedOperationException("Implement it!");
     }
 
     public static Map<Character, Integer> countChars(String text) {
-        throw new UnsupportedOperationException("Implement it!");
+        HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
+        char[] strArray = text.toCharArray();
+        for (char c : strArray) {
+            if (charCountMap.containsKey(c)) {
+                charCountMap.put(c, charCountMap.get(c) + 1);
+            } else {
+                charCountMap.put(c, 1);
+            }
+        }
+        return charCountMap;
+//        throw new UnsupportedOperationException("Implement it!");
     }
 
     public static String replaceAllDots(String text) {
-        throw new UnsupportedOperationException("Implement it!");
+        return text.replace(".", ",");
+//        throw new UnsupportedOperationException("Implement it!");
     }
 
     public static String reverse(String text) {
-        throw new UnsupportedOperationException("Implement it!");
+        return (new StringBuilder(text).reverse().toString());
+//        throw new UnsupportedOperationException("Implement it!");
     }
 }
+
