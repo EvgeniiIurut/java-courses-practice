54c4feab1d1f6a4d6002e25efc624831f4039952
EvgeniiIurut <65227752+EvgeniiIurut@users.noreply.github.com>
2021-10-07 19:24:48 +0300

    Add files via upload

--- src/main/java/edu/java/course/core/task_01/StringUtils.java
+++ src/main/java/edu/java/course/core/task_01/StringUtils.java
@@ -7,48 +7,36 @@ public class StringUtils {
 
     public static String normalize(String value) {
         return value.toLowerCase();
-//        throw new UnsupportedOperationException("Implement it!");
     }
 
     public static List<String> splitOnLines(String text) {
-        System.out.println("new ArrayList<>(Arrays.asList(text.split(\"\\n\")))");
-        return new ArrayList<>(Arrays.asList(text.split("\n")));
-
-//        throw new UnsupportedOperationException("Implement it!");
+        return Arrays.asList(text.split("\n"));
     }
 
     public static String mergeToSingleString(List<String> lines) {
         StringBuilder stringBuilder = new StringBuilder();
-        Iterator<String> iterator = lines.iterator();
-        while (iterator.hasNext()) {
-            stringBuilder.append(iterator.next());
+        for (String str : lines
+        ) {
+            stringBuilder.append(str);
         }
-        return stringBuilder.toString().trim();
-//        throw new UnsupportedOperationException("Implement it!");
+        return stringBuilder.toString();
     }
 
     public static Map<Character, Integer> countChars(String text) {
         HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
         char[] strArray = text.toCharArray();
         for (char c : strArray) {
-            if (charCountMap.containsKey(c)) {
-                charCountMap.put(c, charCountMap.get(c) + 1);
-            } else {
-                charCountMap.put(c, 1);
-            }
+            charCountMap.compute(c, (key, value) -> value == null ?  1 : ++value);
         }
         return charCountMap;
-//        throw new UnsupportedOperationException("Implement it!");
     }
 
     public static String replaceAllDots(String text) {
         return text.replace(".", ",");
-//        throw new UnsupportedOperationException("Implement it!");
     }
 
     public static String reverse(String text) {
-        return (new StringBuilder(text).reverse().toString());
-//        throw new UnsupportedOperationException("Implement it!");
+        return new StringBuilder(text).reverse().toString();
     }
 }
 
